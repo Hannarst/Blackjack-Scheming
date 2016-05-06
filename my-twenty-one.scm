2487d4b651df2fc69bd257ea43d2fcc802bcc777
Anna Borysova <annasborysova@gmail.com>
2016-05-06 11:15:23 +0200

    q1 best hand

--- my-twenty-one.scm
+++ my-twenty-one.scm
@@ -3,7 +3,7 @@
 ;;;;                 April 2016                            ;;;;
 ;;;;        <joe blogg's> solutions <stdnum000>            ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;  
+;;
 ;; This depends on "cs3-black-jack.scm"
 ;;
 
@@ -16,9 +16,20 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;  Question 1.  Code for "best-hand"
 
+;;; is ace in hand dshc
+(define (ace-present hand)
+ (or (memq '(A d) hand) (memq '(A s) hand) (memq '(A h) hand) (memq '(A c) hand))
+)
+
 ;; Dummy Version of best-hand
 (define (best-hand hand)
-   (min-val hand))
+  (if ace-present(hand)
+    (if (> (+ (min-val hand) 10) 21)
+      (min-val hand)
+      (+ (min-val hand) 10)
+    )
+    (min-val hand)
+    ))
 
 ;; Best Value of Hand
 ;; [add description and test -cases]
@@ -26,6 +37,11 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;  Question 2.  "stop-at"
+
+(define (stop-at stop-num)
+	(lambda (customer-hand-so-far dealer-up-card)
+    (< (best-hand customer-hand-so-far) stop-num)
+    ))
 ;; [add description and test -cases]
 
 
@@ -59,10 +75,9 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;   Question 8.   interactive 
+;;;;;   Question 8.   interactive
 ;; [add description and user insctructions]
 
 ; function to get the input returns #t if the user types y otherwise #f
 (define (hit-me?)
   (eq? (read) 'y))
-
